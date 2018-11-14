b15efb6c8e5e0b881d6b902a89a16bd7be6fefe0
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-13 19:33:27 -0700

    Update backend/index.php

--- backend/index.php
+++ backend/index.php
@@ -9,7 +9,7 @@
       $mylevel = mysqli_real_escape_string($db, $_POST['level']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM users WHERE 'name' = '$myusername'and 'level' = '$mylevel' and 'password' = '$mypassword'";
+      $sql = "SELECT id FROM users WHERE name = '$myusername'and level = '$mylevel' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
