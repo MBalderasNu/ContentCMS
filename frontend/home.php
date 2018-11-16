45ffa7dc0d9c8a0b1f30fd485a930cd0feade3c5
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-15 17:53:19 -0700

    different approach

--- frontend/home.php
+++ frontend/home.php
@@ -1,26 +1,12 @@
-<?php 
-//include database connection
-include 'dbconfig.php';
-
-//select the specific database record to update
-$query = "SELECT * FROM mainpages WHERE id = 1";
-$result = mysqli_query($db,$query);
-$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-//assign the result to certain variable so our html will be filled up with values
-$id = $row['id'];
-$title = $row['title'];
-$content = $row['content'];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <?php echo $title; ?>
+    <title>Document</title>
 </head>
-<body>   
+<body>
+    
 </body>
 </html>
