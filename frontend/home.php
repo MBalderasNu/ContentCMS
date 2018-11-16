58ebbbe9c97937a8a86b7ed6c545928d40d2dba3
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-15 17:43:45 -0700

    Update home.php

--- frontend/home.php
+++ frontend/home.php
@@ -20,7 +20,6 @@ $content = $row['content'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <?php echo $title; ?>
-   <?php echo "hey come on";?>
 </head>
 <body>   
 </body>
