aa0bf5e7fed583ccf4642f8cc5d0c74a221f4613
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-18 12:56:14 -0700

    Update admindata.php
    setting up displaying data on admindata page

--- backend/admindata.php
+++ backend/admindata.php
@@ -1,2 +1,11 @@
+<h3>Main Pages</h3>
 
+<table class="mainpagetable">
+  <tr><th>Id</th><th>Name</th><th>Content</th><th></th><th></th>
+  </tr>
+
+<h3>Sub Pages</h3>
+<table class="subpagetable">
+  <tr><th>Id</th><th>Name</th><th>Content</th><th></th><th></th>
+  </tr>
 
