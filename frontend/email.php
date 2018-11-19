c9fe1f1dfa53273ac65da0ff1e711e80590d949e
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-18 22:14:03 -0700

    relative paths
    joesph added them in, from his branch just doing it here to avoid conflict merges

--- frontend/email.php
+++ frontend/email.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="/ContentProject/ContentCMS/frontend/scripts/emailpagescript.js"></script>
+    <script src="./scripts/emailpagescript.js"></script>
     <h1><div id='title0'></div></h1>
 </head>
 <body>
