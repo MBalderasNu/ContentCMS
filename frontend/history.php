d441accc473a0ad3fc319db369c0f7daa6758c40
MBalderasNu <mbalderas@student.neumont.edu>
2018-11-18 22:22:10 -0700

    added thing to style sheet

--- frontend/history.php
+++ frontend/history.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="./scripts/historypagescript.js"></script>
+    <link rel='stylesheet' href='./styles/theme1.css'>
     <h1><div id='title0'></div></h1>
 </head>
 <body>
